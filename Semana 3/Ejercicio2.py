4b75add787833d24f9310505b55a3a38096cfddf
Isaac <isaacruizse@gmail.com>
2023-02-04 09:32:10 -0600

    sem 3

---
+++ Semana 3/Ejercicio2.py
@@ -0,0 +1,18 @@
+num1 = float(input("Digite el primer numero: "))
+
+while num1 == 0:
+    num1 = float(input("Digite un numero diferente de 0: "))
+
+num2 = float(input("Digite el Segundo numero: "))
+
+while num2 == 0:
+    num2 = float(input("Digite un numero diferente de 0: "))
+
+print("La suma es: ", num1 + num2)
+print("La resta es: ", num1 - num2)
+print("La multiplicacion es: ", num1 * num2)
+print("La division es: ", num1 / num2)
+
+
+
+
