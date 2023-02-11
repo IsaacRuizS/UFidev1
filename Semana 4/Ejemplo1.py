d2948a7243f1bb521b19afebf6e169fd0aebd194
Laboratorios <isaacruizse@gmail.com>
2023-02-11 08:33:42 -0600

    sem 4

---
+++ Semana 4/Ejemplo1.py
@@ -0,0 +1,7 @@
+opcion = int(input("Ingrese la opcion:\n 1. 150ML\n 2. 600ML\n 3. 1000ML\n"))
+if opcion == 1:
+    print("Paga 350 colones")
+elif opcion ==2:
+    print("Paga 850 colones")
+elif opcion == 3:
+    print("Paga 1250 colones")
---
+++ Semana 4/Ejemplo2.py
@@ -0,0 +1,6 @@
+edad= int(input("Digite la edad: "))
+
+if edad < 3 or edad > 65:
+    print("No paga entrada")
+if edad >= 3 and edad <= 65:
+    print("Paga 1500 colones")
