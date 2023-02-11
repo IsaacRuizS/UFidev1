3fe989d5ca53dabf099c888add734439326dc4c2
Laboratorios <isaacruizse@gmail.com>
2023-02-11 10:59:51 -0600

    sem 4

---
+++ Semana 4/Ejercicio5.py
@@ -0,0 +1,28 @@
+num1 = int(input("Ingrese el numero uno: "))
+num2 = int(input("Ingrese el numero dos: "))
+num3 = int(input("Ingrese el numero tres: "))
+num4 = int(input("Ingrese el numero cuatro: "))
+
+if num1>num2 and num1>num3 and num1>num4:
+    print("numero mayor: ", num1)
+elif num2>num1 and num2>num3 and num2>num4:
+    print("numero mayor: ", num2)
+elif num3>num2 and num3>num3 and num3>num4:
+    print("numero mayor: ", num3)
+else:
+    print("numero mayor: ", num4)
+
+
+
+#if num1 > num2:
+#       if num1 > num3:
+#          if num1 > num4:
+#           print("El numero mayor es: ", num1)
+#elif num2 > num3:
+#       if num2 > num4:
+#   print("El numero mayor es: ", num2)
+#elif num3 > num4:
+#   print("El numero mayor es: ", num3)
+#else:
+#    print("El numero mayor es: ", num4)
+
