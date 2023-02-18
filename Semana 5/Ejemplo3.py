6c7b00bace6aa14abfd6184bfec89bcbc962631a
Isaac <isaacruizse@gmail.com>
2023-02-18 09:11:08 -0600

    sem 5

---
+++ Semana 5/Ejemplo3.py
@@ -0,0 +1,3 @@
+for i in range(1,100):
+    if(100-i) % 5 == 0:
+        print(100-i)
---
+++ Semana 5/Ejemplos.py
@@ -0,0 +1,3 @@
+tabla = int(input("Tabla de multiplicar"))
+for i in range(1,10):
+    print(tabla, "x", i, "=", i*tabla)
