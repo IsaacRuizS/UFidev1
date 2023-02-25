a3367664c67e7b20ba82028ae89cda72afe156c8
Laboratorios <isaacruizse@gmail.com>
2023-02-25 08:40:28 -0600

    Juegos de progra

--- Semana 6/JuegosPrograIsaacRuiz.py
+++ Semana 6/JuegosPrograIsaacRuiz.py
@@ -1,1 +1,28 @@
-vuelta1= int(input("Digite el timepo por vuelta "))
+#inicializan las variables para realizar la suma de cada uno de los tiempos de pits y vueltas
+sumaVuelta = 0
+sumaPits= 0
+
+#se recorre la cantidad de vueltas que se correran
+for indice in range(1,6):
+    #se solicita el tiempo por vuelta y por pits
+    print("Vuelta numero: ",indice)
+    vuelta1 = int(input("Digite el timepo la vuelta: "))
+    pits1 = int(input("Ingrese el tiempo de pits: "))
+    #A las variables inicializadas anteriormente se le suman los valores por vuelta y pits ingresados
+    sumaVuelta += vuelta1
+    sumaPits += pits1
+
+#se realiza el promedio por vuelta dividiendo la suma de los tiempos por vuelta dividido entre 5 que es la cantidad de vueltas
+promedioVuelta = sumaVuelta//5
+#Se realiza el promedio por pit por vuelta sumando los tiempos de los pits dividido entre 5
+promedioPits = sumaPits//5
+#se calcula el porcentaje total se calcula multiplicando 100 por la suma de pits dividido entre la suma de las vueltas
+porcentajeTiempo = 100*sumaPits//sumaVuelta
+
+#Se muestran todos los datos calculados y solicitados
+
+print("El primedio de las vueltas: ", promedioVuelta,"\n El promedio de los pits es: ", promedioPits)
+print("El porcentaje de tiempo de pits correspondiente al tiempo recorrido es: ", porcentajeTiempo)
+
+
+
